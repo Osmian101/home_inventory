5fa99b01c74a9536948e0096a156b5ff7c0e7868
Osmian <r093r25@gmail.com>
2023-09-18 23:31:34 -0400

    updated design and reads from file into a stream

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 // setup argv
 int main()
 {
+	string file_path = "inventory.csv";
+	ifstream inventory_file;
+	inventory_file.open(file_path);
+	if (inventory_file.is_open())
+	{
+		// read data char by char, split data on comma, new entry by line
+		// replace cout with item struct
+		cout << inventory_file.rdbuf();
+	}
+	inventory_file.close();
+	return 0;
 	// open csv
 	// switch on second arg
 }
